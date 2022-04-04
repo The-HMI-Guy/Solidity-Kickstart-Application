b72f4cf26d626c4465f72e59f4541ff9141a6e57
Ray Ortiz <oray@bgsu.edu>
2022-04-04 14:52:03 -0400

    Changes to index/web3

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -57,6 +57,11 @@
 /******/ 	__webpack_require__.i = [];
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	!function() {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	}();
+/******/ 	
 /******/ 	/* webpack/runtime/chunk loaded */
 /******/ 	!function() {
 /******/ 		var deferred = [];
@@ -131,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "49950620fcbaab0a"; }
+/******/ 		__webpack_require__.h = function() { return "73ac2c9755371879"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
