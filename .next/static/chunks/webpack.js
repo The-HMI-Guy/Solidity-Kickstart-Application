304f4642723239eada212dde8378932f1da37666
Ray Ortiz <oray@bgsu.edu>
2022-04-05 07:36:21 -0400

    getInitialProps

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f5cc31edee9a523a"; }
+/******/ 		__webpack_require__.h = function() { return "01e609c909c01b53"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
