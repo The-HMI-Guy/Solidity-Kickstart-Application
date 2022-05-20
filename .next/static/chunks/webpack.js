5d62eb2c20f4bc5fab0a5943c378ec96f2dd1244
Ray Ortiz <oray@bgsu.edu>
2022-05-19 22:09:44 -0400

    Add server.js and routes.js

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a73ae097ea666e7c"; }
+/******/ 		__webpack_require__.h = function() { return "656bf1d0b82e46dc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
