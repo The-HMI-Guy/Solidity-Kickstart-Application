5ad1f9297965f32e35e8d1f386733f6d5b06a4d3
Ray Ortiz <oray@bgsu.edu>
2022-05-28 20:37:54 -0400

    Accessing a campaign

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4e86922a428b7fcd"; }
+/******/ 		__webpack_require__.h = function() { return "a99ff35d43e22483"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
